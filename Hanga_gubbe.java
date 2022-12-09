c292e08fd13a00766dc4a1ec059de55357a06105
L1nu5-21 <94972443+L1nu5-21@users.noreply.github.com>
2022-12-09 10:27:51 +0100

    Initial commit

---
+++ Hanga_gubbe.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hanga_gubbe {
+    public static void main(String[] args) {
+        // ord inmatning //
+        Scanner in = new Scanner(System.in);
+        String gubbe = in.next();
+
+
+    }
+}
